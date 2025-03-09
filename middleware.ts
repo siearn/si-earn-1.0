b02390fff21ea8df7677f78f9c0759d295af23dd
siearn <contactsiearn@gmail.com>
2025-03-09 17:18:25 -0400

    Replaced all files with new version

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
-import { authMiddleware } from "@clerk/nextjs"
+import { clerkMiddleware } from "@clerk/nextjs/server"
 
-export default authMiddleware({
-  publicRoutes: ["/", "/login", "/signup", "/api/webhooks(.*)"],
-})
+export default clerkMiddleware()
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
